82ca70e868ce52d221ffe59c071b9ca8e73e0ef8
Joasia662 <60195641+Joasia662@users.noreply.github.com>
2021-11-29 17:06:01 +0100

    Added my project to github repository

---
+++ automata_One_dimensional/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace automata_One_dimensional
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Start = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.downUP_width = new System.Windows.Forms.NumericUpDown();
+            this.downUp_iteration_count = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cb_rodzaj = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.downUP_width)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downUp_iteration_count)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.Navy;
+            this.Start.FlatAppearance.BorderSize = 0;
+            this.Start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Start.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Start.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.Start.Location = new System.Drawing.Point(28, 84);
+            this.Start.Margin = new System.Windows.Forms.Padding(2);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(208, 76);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.downUP_width);
+            this.panel1.Controls.Add(this.downUp_iteration_count);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.cb_rodzaj);
+            this.panel1.Controls.Add(this.Start);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(486, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(258, 440);
+            this.panel1.TabIndex = 2;
+            // 
+            // downUP_width
+            // 
+            this.downUP_width.Location = new System.Drawing.Point(156, 315);
+            this.downUP_width.Name = "downUP_width";
+            this.downUP_width.Size = new System.Drawing.Size(80, 20);
+            this.downUP_width.TabIndex = 13;
+            // 
+            // downUp_iteration_count
+            // 
+            this.downUp_iteration_count.Location = new System.Drawing.Point(156, 272);
+            this.downUp_iteration_count.Name = "downUp_iteration_count";
+            this.downUp_iteration_count.Size = new System.Drawing.Size(80, 20);
+            this.downUp_iteration_count.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(152, 292);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "szerokosc";
+            // 
+            // cb_rodzaj
+            // 
+            this.cb_rodzaj.FormattingEnabled = true;
+            this.cb_rodzaj.Items.AddRange(new object[] {
+            "30",
+            "60",
+            "90",
+            "120",
+            "150"});
+            this.cb_rodzaj.Location = new System.Drawing.Point(28, 291);
+            this.cb_rodzaj.Margin = new System.Windows.Forms.Padding(2);
+            this.cb_rodzaj.Name = "cb_rodzaj";
+            this.cb_rodzaj.Size = new System.Drawing.Size(110, 21);
+            this.cb_rodzaj.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 269);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Rodzaj";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(152, 249);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ilość iteracji";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Navy;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.Location = new System.Drawing.Point(28, 171);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 76);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // pic
+            // 
+            this.pic.BackColor = System.Drawing.Color.AliceBlue;
+            this.pic.Location = new System.Drawing.Point(17, 19);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(450, 400);
+            this.pic.TabIndex = 15;
+            this.pic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(746, 438);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Automaty 1D - Joanna Cepuch";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.downUP_width)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downUp_iteration_count)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cb_rodzaj;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown downUP_width;
+        private System.Windows.Forms.NumericUpDown downUp_iteration_count;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pic;
+    }
+}
+
---
+++ automata_One_dimensional/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace automata_One_dimensional
+{
+    public partial class Form1 : Form
+    {
+
+		//***************************** LOGIC *************************************
+		static string convert_to_binary(String input)
+		{
+			string result;
+
+			int num = Convert.ToInt32(input);
+			result = "";
+			while (num > 1)
+			{
+				int remainder = num % 2;
+				result = Convert.ToString(remainder) + result;
+				num /= 2;
+			}
+			result = Convert.ToString(num) + result;
+
+
+			return result;
+		}
+
+		static DataTable get_DataTable_of_binary_rule(string output)
+		{
+			DataTable rule_binary = new System.Data.DataTable();
+			rule_binary.Columns.Add("numbers", typeof(Int32));
+
+			int temp_int_to_make_right_binary_format = 8;
+			while (output.Length != temp_int_to_make_right_binary_format)
+			{
+				rule_binary.Rows.Add(0);
+				temp_int_to_make_right_binary_format--;
+			}
+
+			for (int i = 0; i < output.Length; i++)
+			{
+				rule_binary.Rows.Add(Convert.ToInt32(new string(output[i], 1)));
+			}
+
+
+			
+
+			return rule_binary;
+		}
+
+
+		static int parent_values_into_child(int one, int two, int three)
+		{
+
+			if (!(one < 0 || one > 1 || two < 0 || two > 1 || three < 0 || three > 1))
+			{
+				string status = Convert.ToString(one) + Convert.ToString(two) + Convert.ToString(three);
+				int output = Convert.ToInt32(status, 2);
+				return output;
+			}
+			else
+			{
+				//nothing 
+				return 0;
+			}
+
+		}
+
+		static int[,] createArray(int y, int x)
+		{
+
+			int[,] initMatrix = new int[y, x];
+			for (int i = 0; i < y; i++)
+			{
+				for (int j = 0; j < x; j++)
+				{
+					initMatrix[i, j] = 0;
+				}
+			}
+
+			int mid = x / 2;
+			initMatrix[0, mid] = 1;
+			return initMatrix;
+		}
+
+
+
+		//****************************END OF LOGIC ************************************************
+
+		public Form1()
+		{
+
+			InitializeComponent();
+
+			downUP_width.Maximum = 450;
+			downUp_iteration_count.Maximum = 400;
+
+			downUP_width.Minimum = 0;
+			downUp_iteration_count.Minimum = 0;
+
+			downUP_width.Increment = 1;
+			downUp_iteration_count.Increment = 1;
+
+			downUP_width.Value = 100;
+			downUp_iteration_count.Value = 100;
+
+			
+		}
+
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+
+
+
+			int width = Convert.ToInt32(downUP_width.Value);
+			int iteration_count = Convert.ToInt32(downUp_iteration_count.Value);
+			string rule_dec = cb_rodzaj.SelectedItem.ToString();
+
+
+			if (rule_dec == "30") { rule_dec = 120.ToString(); }
+			else if (rule_dec == "120") { rule_dec = 30.ToString(); }
+
+			string rule_bin = convert_to_binary(rule_dec);
+			DataTable rule_numbers = get_DataTable_of_binary_rule(rule_bin);
+
+
+			int[,] my_matrix = createArray(iteration_count, width);
+
+			for (int i = 0; i < iteration_count - 1; i++)
+			{
+				for (int j = 0; j < width; j++)
+				{
+
+
+					int left = ((j == 0) ? (width - 1) : (j - 1));
+					int right = ((j == (width - 1)) ? (0) : (j + 1));
+
+					int child_index = parent_values_into_child(my_matrix[i, left], my_matrix[i, j], my_matrix[i, right]);
+					my_matrix[i + 1, j] = Convert.ToInt32(rule_numbers.Rows[child_index]["numbers"]);
+				}
+			}
+
+			int x = (450 - width) / 2;
+			int y = 0;
+			pic.Image = new Bitmap(pic.Width, pic.Height);
+			for (int i = 0; i < my_matrix.GetUpperBound(0) + 1; i++)
+				{
+					for (int j = 0; j <= my_matrix.GetUpperBound((my_matrix.Rank) - 1); j++)
+					{
+						int value = my_matrix[i, j];
+						if (value == 1)
+						{
+							((Bitmap)pic.Image).SetPixel(x, y, Color.FromArgb(0, 0, 0));
+						}
+
+						x++;
+					}
+					y++;
+				x = (450 - width) / 2;
+			}
+		}
+
+			
+
+
+
+
+	}
+
+
+
+
+}
+
+        
+        
+    
+
